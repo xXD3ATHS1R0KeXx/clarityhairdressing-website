9b4166f0d7f70dca4335835e9c774f03e3c13c8f
Cooper Tuapawa <coopertuapawa@MacBook-Air.localdomain>
2022-03-07 01:02:06 +1030

    trying to optimise website loading

--- components/button.js
+++ components/button.js
@@ -13,6 +13,6 @@ const Button = tw.button`
 
 export default function button({ children }){
     return(
-        <Button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">{ children }</Button>
+        <Button className="bg-clarityGold hover:bg-buttonGold text-white font-bold py-2 px-4 rounded">{ children }</Button>
     )
 }
