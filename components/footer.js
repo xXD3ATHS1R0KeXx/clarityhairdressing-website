3d6ef6a7309c4abfa7c664b4c8c96ed1a8dd2979
Cooper Tuapawa <coopertuapawa@MacBook-Air.localdomain>
2022-03-08 12:18:36 +1030

    Updated code, Added footer.js component still needing work updated herosection.js and added footer to the index.js file

--- components/footer.js
+++ components/footer.js
@@ -1,7 +1,77 @@
 import React from 'react'
+import Image from 'next/image';
+import clarityLogo from "../public/logo.png"
 
-export default function footer() {
-  return (
-    <div>footer</div>
-  )
+
+export default function footer(){
+    return(
+		<>
+			<div className="bg-clarityGold h-1/2 w-full flex md:flex-row flex-col justify-around items-start p-20 mt-5">
+				<div className="pl-5">
+					<ul>
+                        <Image src={clarityLogo} width={200} height={200} />
+						<div className="flex gap-6 pb-5">
+						</div>
+					</ul>
+				</div>
+				<div className="p-5">
+					<ul>
+						<p className="text-gray-800 font-bold text-2xl pb-4">Product</p>
+						<li className="text-gray-500 text-md pb-2 font-semibold hover:text-blue-600 cursor-pointer">
+							Stocks
+						</li>
+						<li className="text-gray-500 text-md pb-2 font-semibold hover:text-blue-600 cursor-pointer">
+							Futures & Options
+						</li>
+						<li className="text-gray-500 text-md pb-2 font-semibold hover:text-blue-600 cursor-pointer">
+							Mutual Funds
+						</li>
+						<li className="text-gray-500 text-md pb-2 font-semibold hover:text-blue-600 cursor-pointer">
+							Fixed deposits
+						</li>
+					</ul>
+				</div>
+				<div className="p-5">
+					<ul>
+						<p className="text-gray-800 font-bold text-2xl pb-4">Company</p>
+						<li className="text-gray-500 text-md pb-2 font-semibold hover:text-blue-600 cursor-pointer">
+							About
+						</li>
+						<li className="text-gray-500 text-md pb-2 font-semibold hover:text-blue-600 cursor-pointer">
+							Products
+						</li>
+						<li className="text-gray-500 text-md pb-2 font-semibold hover:text-blue-600 cursor-pointer">
+							Pricing
+						</li>
+						<li className="text-gray-500 text-md pb-2 font-semibold hover:text-blue-600 cursor-pointer">
+							Careers
+						</li>
+						<li className="text-gray-500 text-md pb-2 font-semibold hover:text-blue-600 cursor-pointer">
+							Press & Media
+						</li>
+					</ul>
+				</div>
+				<div className="p-5">
+					<ul>
+						<p className="text-gray-800 font-bold text-2xl pb-4">Support</p>
+						<li className="text-gray-500 text-md pb-2 font-semibold hover:text-blue-600 cursor-pointer">
+							Contact
+						</li>
+						<li className="text-gray-500 text-md pb-2 font-semibold hover:text-blue-600 cursor-pointer">
+							Support Portals
+						</li>
+						<li className="text-gray-500 text-md pb-2 font-semibold hover:text-blue-600 cursor-pointer">
+							List Of Charges
+						</li>
+						<li className="text-gray-500 text-md pb-2 font-semibold hover:text-blue-600 cursor-pointer">
+							Downloads & Resources
+						</li>
+						<li className="text-gray-500 text-md pb-2 font-semibold hover:text-blue-600 cursor-pointer">
+							Videos
+						</li>
+					</ul>
+				</div>
+			</div>
+		</>
+	);
 }
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 import HeroSection from "../components/herosection";
 import Navbar from "../components/navigationbar";
 import Aboutsection from "../components/aboutsection"
+import Footer from "../components/footer"
 
 export default function Home() {
 	return (
@@ -14,6 +15,7 @@ export default function Home() {
 			<Navbar/>
 			<HeroSection />
 			<Aboutsection />
+			<Footer />
 		</div>
 	);
 }
