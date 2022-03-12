88a97d7a0376145712b6a2414d9a0a3855176d96
Cooper Tuapawa <coopertuapawa@MacBook-Air.localdomain>
2022-03-12 11:37:25 +1030

    Updated code, got rid of cards component and went with more simple approach

--- components/cards.js
+++ components/cards.js
@@ -1,13 +1,20 @@
 import React from "react"
 import Image from "next/image"
+import wings from "../public/Wings.jpeg"
 
 
 export default function cards(props){
     return(
-        <div class="max-w-sm rounded overflow-hidden shadow-lg">
-            <div class="px-6 py-4">
-                <div class="font-bold text-xl mb-2">{ props.title }</div>
-                <p class="text-gray-700 text-base"> { props.desc } </p>
+        <div class="flex justify-center">
+            <div class="rounded-lg shadow-lg bg-white max-w-sm">
+                <Image src={"/"+props.src} layout="fill" objectFit="contain"/>
+                <div class="p-6">
+                    <h5 class="text-gray-900 text-xl font-medium mb-2">Card title</h5>
+                    <p class="text-gray-700 text-base mb-4">
+                        Some quick example text to build on the card title and make up the bulk of the card's
+                        content.
+                    </p>                
+                </div>
             </div>
         </div>
     )
