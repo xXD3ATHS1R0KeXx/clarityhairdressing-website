01fa0021bccb6c176d8abd843d77bf14f76c580d
xXD3ATHS1R0KeXx <53144014+xXD3ATHS1R0KeXx@users.noreply.github.com>
2022-04-04 20:02:54 +0930

    Updated code
    Updated herosection.js and added new business to the otherbuisnesses.js file added their logo to the logo folder

--- components/herosection.js
+++ components/herosection.js
@@ -5,23 +5,28 @@ import Button from "./button"
 
 export default function HeroSection() {
 	return (
-		<div className="flex justify-center items-center text-center z-0 animate-fade-in-down pt-24 md:pt-10">
+		<div className="flex justify-center items-center text-center z-0 animate-fade-in-down pt-24 md:pt-32 md:pt-10">
         <Image src={wings} width={800} height={800} priority />
         <div className="absolute">
-          <h1 className=" text-gray-900 text-7xl md:text-8xl font-italianno mt-28">
+          <h1 className=" text-gray-900 text-7xl md:text-8xl font-italianno mt-40">
             Clarity
           </h1>
           <h2 className=" text-gray-900 md:text-4xl text-3xl font-italianno tracking-wider">
             By James Henderson Hairdressing
           </h2>
+          { /*
           <div>
             <p className="mt-3 px-5 text-justifty font-gayathri text-gray-500 md:mt-5 md:text-lg md:max-w-xl md:mx-auto md:mt-5 md:text-xl lg:mx-0 animate-fade-in-down">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed feugiat tempus sem sit amet sodales. 
             Praesent aliquet molestie augue nec laoreet. Cras at dui ac neque eleifend condimentum. 
             </p>
           </div>
+          */}
           <div className="mt-7">
-            <Button src="https://google.com">Book now</Button>
+            <Button src="https://bookings.gettimely.com/jameshendersonhairdressing/bb/book">Book now</Button>
+          </div>
+          <div className="mt-5 font-gayathri text-gray-500">
+            <p className="tracking-wider"> Please note some services are only available through booking on Instagram at @clarity_by_jameshenderson </p>
           </div>
       </div>
     </div>
--- pages/otherbusinesses.js
+++ pages/otherbusinesses.js
@@ -39,7 +39,7 @@ export default function otherbusinesses() {
                                     <p className="mb-4 text-justify font-gayathri">Khiara is the face behind Let’s Get Lashed.  She has owned her own lash and brow business for 5 years and still has the same passion as day 1. She specialises in all lash extension services including classNameics, hybrids and volumes, you name it she can do it. She also has you covered for your lash lifts and tints.
                                         Stand out with beautiful brows ~ Khiara is also skilled in brow waxing and takes pleasure in creating the perfect shape to suit you. With laminations being a hot trend she can give you the bold brows you always dreamed of and tops it of with a hybrid tint giving you the perfect definition. To book in with Khiara click on the link in her Instagram bio.</p>
                                     <span className="inline-flex">
-                                        <a className="button--svg" href="https://www.instagram.com/" target="_blank" rel="noreferrer">
+                                        <a className="button--svg" href="https://instagram.com/letsgetlasheddd?utm_medium=copy_link" target="_blank" rel="noreferrer">
                                             <svg xmlns="http://www.w3.org/2000/svg" width="46" height="46" viewBox="0 0 46 46" role="img" aria-labelledby="instagram-icon">
                                                 <title id="instagram-icon">Instagram</title>
                                                 <path fill="#d5bfa4" d="M29.76 29.03v-7.373h-1.537c.152.48.23.975.23 1.49 0 .958-.243 1.838-.73 2.647-.485.807-1.146 1.447-1.98 1.918-.834.47-1.744.705-2.73.705-1.495 0-2.773-.514-3.835-1.543-1.062-1.027-1.593-2.27-1.593-3.726 0-.517.076-1.013.228-1.49H16.21v7.373c0 .2.065.37.2.5.13.14.296.2.494.2H29.07c.188 0 .352-.06.488-.2s.202-.3.202-.49zm-3.233-6.064c0-.94-.343-1.743-1.03-2.406-.686-.664-1.515-.996-2.486-.996-.96 0-1.78.332-2.47.996-.68.663-1.03 1.466-1.03 2.406 0 .942.35 1.743 1.03 2.407s1.51.996 2.48.996c.975 0 1.8-.34 2.49-1s1.03-1.47 1.03-2.41zm3.233-4.097v-1.88c0-.22-.076-.4-.23-.56-.15-.158-.336-.235-.556-.235h-1.98c-.22 0-.406.08-.558.233-.15.155-.228.34-.228.552v1.876c0 .22.076.404.228.556s.337.23.558.23h1.98c.22 0 .406-.078.557-.23.16-.15.23-.338.23-.558zm1.98-2.37v12.99c0 .61-.22 1.14-.66 1.58-.44.44-.967.66-1.582.66H16.502c-.614 0-1.142-.22-1.582-.66-.44-.44-.66-.97-.66-1.586V16.5c0-.614.22-1.142.66-1.582.44-.44.967-.66 1.582-.66h12.996c.615 0 1.14.22 1.582.66.44.44.66.967.66 1.58z"/>
@@ -63,7 +63,7 @@ export default function otherbusinesses() {
                                         We can’t wait for you all to meet Tiffany; and to book in for your next set of gorgeous lash extensions with one of the best in the industry.
                                     </p>
                                     <span className="inline-flex">
-                                        <a className="button--svg" href="https://www.instagram.com/" target="_blank" rel="noreferrer">
+                                        <a className="button--svg" href="https://instagram.com/youandeye.beauty_?utm_medium=copy_link" target="_blank" rel="noreferrer">
                                             <svg xmlns="http://www.w3.org/2000/svg" width="46" height="46" viewBox="0 0 46 46" role="img" aria-labelledby="instagram-icon">
                                                 <title id="instagram-icon">Instagram</title>
                                                 <path fill="#d5bfa4" d="M29.76 29.03v-7.373h-1.537c.152.48.23.975.23 1.49 0 .958-.243 1.838-.73 2.647-.485.807-1.146 1.447-1.98 1.918-.834.47-1.744.705-2.73.705-1.495 0-2.773-.514-3.835-1.543-1.062-1.027-1.593-2.27-1.593-3.726 0-.517.076-1.013.228-1.49H16.21v7.373c0 .2.065.37.2.5.13.14.296.2.494.2H29.07c.188 0 .352-.06.488-.2s.202-.3.202-.49zm-3.233-6.064c0-.94-.343-1.743-1.03-2.406-.686-.664-1.515-.996-2.486-.996-.96 0-1.78.332-2.47.996-.68.663-1.03 1.466-1.03 2.406 0 .942.35 1.743 1.03 2.407s1.51.996 2.48.996c.975 0 1.8-.34 2.49-1s1.03-1.47 1.03-2.41zm3.233-4.097v-1.88c0-.22-.076-.4-.23-.56-.15-.158-.336-.235-.556-.235h-1.98c-.22 0-.406.08-.558.233-.15.155-.228.34-.228.552v1.876c0 .22.076.404.228.556s.337.23.558.23h1.98c.22 0 .406-.078.557-.23.16-.15.23-.338.23-.558zm1.98-2.37v12.99c0 .61-.22 1.14-.66 1.58-.44.44-.967.66-1.582.66H16.502c-.614 0-1.142-.22-1.582-.66-.44-.44-.66-.97-.66-1.586V16.5c0-.614.22-1.142.66-1.582.44-.44.967-.66 1.582-.66h12.996c.615 0 1.14.22 1.582.66.44.44.66.967.66 1.58z"/>
@@ -87,7 +87,42 @@ export default function otherbusinesses() {
                                         their brow hairs and facial features. Whether a natural enhancement or a bold brow is your preference, Bec can create a fabulous semi permanent set of brows All bookings can be made via Instagram.
                                     </p>
                                     <span className="inline-flex">
-                                        <a className="button--svg" href="https://www.instagram.com/" target="_blank" rel="noreferrer">
+                                        <a className="button--svg" href="https://instagram.com/brows_bybec?utm_medium=copy_link" target="_blank" rel="noreferrer">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="46" height="46" viewBox="0 0 46 46" role="img" aria-labelledby="instagram-icon">
+                                                <title id="instagram-icon">Instagram</title>
+                                                <path fill="#d5bfa4" d="M29.76 29.03v-7.373h-1.537c.152.48.23.975.23 1.49 0 .958-.243 1.838-.73 2.647-.485.807-1.146 1.447-1.98 1.918-.834.47-1.744.705-2.73.705-1.495 0-2.773-.514-3.835-1.543-1.062-1.027-1.593-2.27-1.593-3.726 0-.517.076-1.013.228-1.49H16.21v7.373c0 .2.065.37.2.5.13.14.296.2.494.2H29.07c.188 0 .352-.06.488-.2s.202-.3.202-.49zm-3.233-6.064c0-.94-.343-1.743-1.03-2.406-.686-.664-1.515-.996-2.486-.996-.96 0-1.78.332-2.47.996-.68.663-1.03 1.466-1.03 2.406 0 .942.35 1.743 1.03 2.407s1.51.996 2.48.996c.975 0 1.8-.34 2.49-1s1.03-1.47 1.03-2.41zm3.233-4.097v-1.88c0-.22-.076-.4-.23-.56-.15-.158-.336-.235-.556-.235h-1.98c-.22 0-.406.08-.558.233-.15.155-.228.34-.228.552v1.876c0 .22.076.404.228.556s.337.23.558.23h1.98c.22 0 .406-.078.557-.23.16-.15.23-.338.23-.558zm1.98-2.37v12.99c0 .61-.22 1.14-.66 1.58-.44.44-.967.66-1.582.66H16.502c-.614 0-1.142-.22-1.582-.66-.44-.44-.66-.97-.66-1.586V16.5c0-.614.22-1.142.66-1.582.44-.44.967-.66 1.582-.66h12.996c.615 0 1.14.22 1.582.66.44.44.66.967.66 1.58z"/>
+                                            </svg>
+                                        </a>
+                                    </span>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="p-4">
+                            <div className="h-full flex sm:flex-row flex-col items-center sm:justify-start justify-center text-center sm:text-left">
+                                <div className="flex-shrink-0 rounded-lg  object-cover object-center sm:mb-0 mb-4">
+                                    <Image src="/Logos/beautybybridie.jpg" width={285} height={200} />
+                                </div>
+                                <div className="flex-grow sm:pl-8">
+                                    <h2 className="title-font font-medium text-4xl text-gray-900 font-italianno">Beauty by Bridie</h2>
+                                    <h3 className="mb-3 font-gayathri">Bridie - @beautybybridie  </h3>
+                                    <p className="mb-4 text-justify font-gayathri">Bridie established Beauty by Bridie when she was 16 years old. She has been working as a professional makeup artist in the industry for over 7 years. 
+                                        Beauty by Bridie is a Wild and Organic, #eco-conscious, #sustainable, #cleanbeauty business. Bridie uses all #organic, #vegan products in her kit and is a stockist for @inikaorganic where she has the skincare and makeup lines available for purchase. 
+                                        Beauty by Bridie is not just limited to makeup. Her services include: <br/> <br/>
+                                        - Professional Makeup Applications <br/>
+                                        - Hair Styling <br/>
+                                        - Bridal Parties <br/>
+                                        - 1:1 Lessons <br/>
+                                        - Aspiring MUA Lessons <br/>
+                                        - Organic Facials <br/>
+                                        - Intuitive Massage <br/>
+                                        - Intuitive Movement & Meditation Classes <br/>
+                                        - Personalised Organic Artworks (painted with berries, ochre & other naturally derived plant materials)<br/>
+                                        - Adolescent Youth Mentoring and Empowerment Programs<br/> <br/>
+                                        Bridie purpose is to work with likeminded souls and help them to remember their power. To connect them to their inner goddess and inspire others to be the best version of themselves and chase their dreams and aspirations. Bridie has been working with youth for over 5 years inspiring them to do just that. 
+                                        Not only will you receive your beautiful pampering treatment with Bridie, you will also leave feeling refreshed, recharged and held in a space of unconditional love, joy and deep connection 
+                                    </p>
+                                    <span className="inline-flex">
+                                        <a className="button--svg" href="https://instagram.com/beautybybridie?utm_medium=copy_link" target="_blank" rel="noreferrer">
                                             <svg xmlns="http://www.w3.org/2000/svg" width="46" height="46" viewBox="0 0 46 46" role="img" aria-labelledby="instagram-icon">
                                                 <title id="instagram-icon">Instagram</title>
                                                 <path fill="#d5bfa4" d="M29.76 29.03v-7.373h-1.537c.152.48.23.975.23 1.49 0 .958-.243 1.838-.73 2.647-.485.807-1.146 1.447-1.98 1.918-.834.47-1.744.705-2.73.705-1.495 0-2.773-.514-3.835-1.543-1.062-1.027-1.593-2.27-1.593-3.726 0-.517.076-1.013.228-1.49H16.21v7.373c0 .2.065.37.2.5.13.14.296.2.494.2H29.07c.188 0 .352-.06.488-.2s.202-.3.202-.49zm-3.233-6.064c0-.94-.343-1.743-1.03-2.406-.686-.664-1.515-.996-2.486-.996-.96 0-1.78.332-2.47.996-.68.663-1.03 1.466-1.03 2.406 0 .942.35 1.743 1.03 2.407s1.51.996 2.48.996c.975 0 1.8-.34 2.49-1s1.03-1.47 1.03-2.41zm3.233-4.097v-1.88c0-.22-.076-.4-.23-.56-.15-.158-.336-.235-.556-.235h-1.98c-.22 0-.406.08-.558.233-.15.155-.228.34-.228.552v1.876c0 .22.076.404.228.556s.337.23.558.23h1.98c.22 0 .406-.078.557-.23.16-.15.23-.338.23-.558zm1.98-2.37v12.99c0 .61-.22 1.14-.66 1.58-.44.44-.967.66-1.582.66H16.502c-.614 0-1.142-.22-1.582-.66-.44-.44-.66-.97-.66-1.586V16.5c0-.614.22-1.142.66-1.582.44-.44.967-.66 1.582-.66h12.996c.615 0 1.14.22 1.582.66.44.44.66.967.66 1.58z"/>
